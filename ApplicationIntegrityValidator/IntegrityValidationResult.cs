379b451074e64cdf130dbacda3c85cd223bfe7d2
Nima Zahedi <Nima.zahedee@gmail.com>
2014-01-30 14:46:13 +0330

    Initial commit.

---
+++ ApplicationIntegrityValidator/IntegrityValidationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationIntegrityValidator
+{
+    public class IntegrityValidationResult
+    {
+        public string Description { get; set; }
+        public Exception Exception { get; set; }
+        public bool Succeed { get; set; }
+    }
+}
---
+++ ApplicationIntegrityValidator/IntegrityValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationIntegrityValidator
+{
+    public class IntegrityValidator
+    {
+        public FileIntegrityValidator File(string fileName)
+        {
+            return new FileIntegrityValidator(fileName);
+        }
+    }
+}
