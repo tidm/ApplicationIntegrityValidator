90d0b422de23630493b5fc8ea5a5ef0fd71b6118
Nima Zahedi <Nima.zahedee@gmail.com>
2014-02-01 13:57:05 +0330

    Completed App Config Validator

---
+++ src/ApplicationIntegrityValidator/AppConfigIntegrityValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationIntegrityValidator
+{
+    public class AppConfigIntegrityValidator
+    {
+        public AppSettingsIntegrityValidator AppSettings(string key)
+        {
+            return new AppSettingsIntegrityValidator(key);
+        }
+
+        public ConnectionStringsIntegrityValidator ConnectionStrings(string name)
+        {
+            return new ConnectionStringsIntegrityValidator(name);
+        }
+    }
+}
---
+++ src/ApplicationIntegrityValidator/ConnectionStringsIntegrityValidator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace ApplicationIntegrityValidator
+{
+    public class ConnectionStringsIntegrityValidator : IEnumerable<IntegrityValidationResult>
+    {
+
+        private readonly List<IntegrityValidationResult> _results;
+        private readonly string _name;
+
+        public ConnectionStringsIntegrityValidator(string name)
+        {
+            _name = name;
+            _results = new List<IntegrityValidationResult>();
+        }
+
+        public ConnectionStringsIntegrityValidator Exists()
+        {
+            var result = new IntegrityValidationResult()
+            {
+                Description = string.Format("Ensure connection string with name: {0} exists in web.config", _name),
+                Succeed = ConfigurationManager.ConnectionStrings[_name] != null,
+                Exception = null
+            };
+            _results.Add(result);
+            return this;
+        }
+
+        public ConnectionStringsIntegrityValidator ValueIs(string connectionString)
+        {
+            var result = new IntegrityValidationResult()
+            {
+                Description = string.Format("Ensure connection string with name: {0} has connectionstring: {1} in web.config", _name, connectionString),
+                Succeed = ConfigurationManager.ConnectionStrings[_name].ConnectionString == connectionString,
+                Exception = null
+            };
+            _results.Add(result);
+            return this;
+        }
+
+
+        public IEnumerator<IntegrityValidationResult> GetEnumerator()
+        {
+            return _results.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _results.GetEnumerator();
+        }
+    }
+}
